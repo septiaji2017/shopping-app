025c15f033ab8a57c2f12535aba729728c35bd98
septiaji2017 <septiaji2017@gmail.com>
2024-05-16 21:24:30 +0700

    change route checkout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,7 +7,7 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::home');
 $routes->get('/cart', 'Home::index');
-$routes->get('/form', 'Home::form');
+$routes->get('/checkout', 'Home::form');
 $routes->post('/cart/add/(:num)', 'Home::add_to_cart/$1');
 $routes->post('/cart/clear', 'Home::clear_cart');
 $routes->post('/cart/update_quantity', 'Home::update_quantity');
